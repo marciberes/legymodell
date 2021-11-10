0ac3e64bd8128291a8f99154dd85a7258e13b2b9
Roland Pusztai <prolika99@gmail.com>
2021-11-10 11:59:37 +0100

    Added home view

--- website/views.py
+++ website/views.py
@@ -5,3 +5,11 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def home():
     return render_template("home.html")
+
+@views.route('/ads')
+def ads():
+    return "<h1>ADS TESTING</h1>"
+
+@views.route('/create-ad')
+def create_ad():
+    return "<h1>CREATE-AD TESTING</h1>"
